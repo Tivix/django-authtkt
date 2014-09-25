c8843d6953bb1e88e20c3e2db48d0a32b9cd516a
Peter Thomas <pjthomas404@gmail.com>
2014-09-24 21:47:23 -0700

    removed pdb.

--- authtkt/auth_tkt.py
+++ authtkt/auth_tkt.py
@@ -4,7 +4,6 @@ from codecs import utf_8_encode
 import os, sys
 project = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 paste_dir = '%s%s' % (os.path.dirname(project), '/lib/python2.7/site-packages/paste')
-import pdb; pdb.set_trace()
 sys.path.append(paste_dir)
 import time
 
